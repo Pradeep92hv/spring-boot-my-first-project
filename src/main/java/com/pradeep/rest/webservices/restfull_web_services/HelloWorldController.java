921c7877f1c8bcd4e7e7154a60183f348cceb060
Pradeep92hv <hvpradeep92@gmail.com>
2024-06-05 11:52:20 +0530

    first commit

---
+++ src/main/java/com/pradeep/rest/webservices/restfull_web_services/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.pradeep.rest.webservices.restfull_web_services;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//controller
+@RestController
+public class HelloWorldController {
+
+
+    @GetMapping(path = "/hello-world")
+    public  String helloWorld()
+    {
+        return  "Hello world";
+    }
+}
---
+++ src/main/java/com/pradeep/rest/webservices/restfull_web_services/RestfullWebServicesApplication.java
@@ -0,0 +1,15 @@
+package com.pradeep.rest.webservices.restfull_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServicesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestfullWebServicesApplication.class, args);
+		System.out.println("welcome");
+	}
+
+}
