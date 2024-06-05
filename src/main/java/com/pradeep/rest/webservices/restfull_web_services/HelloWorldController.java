fc22fbfaa788f210233b2be256d0b8381249cadf
Pradeep92hv <hvpradeep92@gmail.com>
2024-06-05 13:50:20 +0530

    step 52 : Connectiong Angular FrontEnd with Restfull API 3 Understanding Obser

--- src/main/java/com/pradeep/rest/webservices/restfull_web_services/HelloWorldController.java
+++ src/main/java/com/pradeep/rest/webservices/restfull_web_services/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.PublicKey;
 
 //controller
+@CrossOrigin(origins ="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
@@ -13,6 +14,13 @@ public class HelloWorldController {
     public  String helloWorld()
     {
         return  "Hello world ";
+    }
+
+
+    @GetMapping(path = "/hello-world-bean")
+    public  HelloWorldBean helloWorldBean()
+    {
+        return new HelloWorldBean("Hello world");
     }
 
     @GetMapping(path = "/hello-world/path-variable/{name}")
