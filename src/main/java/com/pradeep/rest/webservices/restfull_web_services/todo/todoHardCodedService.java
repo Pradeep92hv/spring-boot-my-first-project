a8902547b47e7396d29a2d8ddeeed1ca85c3515b
Pradeep92hv <hvpradeep92@gmail.com>
2024-06-05 17:59:02 +0530

    step 59: Creating  Rest API to delete a Todo 1 create Delete Request Method

--- src/main/java/com/pradeep/rest/webservices/restfull_web_services/todo/todoHardCodedService.java
+++ src/main/java/com/pradeep/rest/webservices/restfull_web_services/todo/todoHardCodedService.java
@@ -23,4 +23,23 @@ public class todoHardCodedService {
     {
         return todos;
     }
+
+
+    public Todo deleteById(long id)
+    {
+        Todo todo=findById(id);
+        if(todo ==null) return  null;
+        todos.remove(todo);
+
+        return  todo;
+    }
+
+    private Todo findById(long id) {
+        for(Todo todo:todos){
+            if(todo.getId()==id){
+                return todo;
+            }
+        }
+        return null;
+    }
 }
